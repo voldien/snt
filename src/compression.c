3f5a73c477492c33a73f7b63a54155c5f16fb885
Voldie <voldiekami@gmail.com>
2017-05-21 08:41:32 +0200

    Minor changes and resolved gzip inflate and deflate function issue.

--- src/compression.c
+++ src/compression.c
@@ -5,7 +5,7 @@
 #include<lz4.h>
 #include<zlib.h>
 
-static const unsigned int compressbound = 2000;
+static unsigned int g_compressbound = 2000;
 
 const char* gs_symcompression[] = {
 		"",
@@ -18,8 +18,10 @@ void sntInitCompression(unsigned int type){
 
 	if(type & SNT_COMPRESSION_LZ4){
 		int err;
-		sntDebugPrintf("Initialize LZ4, %d.\n", compressbound);
-		err = LZ4_compressBound((int)LZ4_COMPRESSBOUND(compressbound));
+		int compbound = (int)LZ4_COMPRESSBOUND(g_compressbound);
+		sntDebugPrintf("Initialize LZ4, %d.\n", compbound);
+		g_compressbound = (unsigned int)compbound;
+		err = LZ4_compressBound(compbound);
 		if(err <= 0){
 			fprintf(stderr, "lz4 failed to initialize with error %d.\n", err);
 			exit(EXIT_FAILURE);
@@ -27,12 +29,14 @@ void sntInitCompression(unsigned int type){
 	}
 	if(type & SNT_COMPRESSION_GZIP){
 		uLong err;
-		sntDebugPrintf("Initialize gzip, %d.\n", compressbound);
-		err = compressBound((uLong)compressbound);
-		if(err < compressbound){
+		err = compressBound((uLong)g_compressbound);
+		g_compressbound = (unsigned int)err;
+		sntDebugPrintf("Initialize gzip, %d.\n", g_compressbound);
+		if(err < g_compressbound){
 			fprintf(stderr, "gzip failed to initialize with error %ld.\n", err);
 			exit(EXIT_FAILURE);
 		}
+
 	}
 }
 
@@ -44,13 +48,13 @@ int sntInflate(unsigned int com, const char* source, char* dest,
 
 	switch(com){
 	case SNT_COMPRESSION_LZ4:
-		inflen = LZ4_decompress_safe(source, dest, slen, compressbound);
+		inflen = LZ4_decompress_safe(source, dest, slen, g_compressbound);
 		sntDebugPrintf("sntInflate, lz4 %u:%d.\n", slen, inflen);
 		assert(inflen >= 0);
 		break;
 	case SNT_COMPRESSION_GZIP:
+		inflen = g_compressbound;
 		err = uncompress((Bytef*)dest, (uLongf*)&inflen, (const Bytef*)source, (uLongf)slen);
-		printf("%d.\n", err);
 		assert(err == Z_OK);
 		sntDebugPrintf("sntInflate, gzip %u:%d.\n", slen, inflen);
 		assert(inflen >= 0);
@@ -65,20 +69,19 @@ int sntInflate(unsigned int com, const char* source, char* dest,
 int sntDeflate(unsigned int com, const char* source, char* dest,
 		unsigned int slen) {
 
-	long int deflen = 0;
+	long int deflen;
 	int err;
 
 	switch(com){
 	case SNT_COMPRESSION_LZ4:
-		deflen = LZ4_compress(source, dest, slen);
+		deflen = LZ4_compress(source, dest, (int)slen);
 		sntDebugPrintf("sntDeflate, lz4 %u:%d.\n", slen, deflen);
 		assert(deflen >= 0);
 		break;
 	case SNT_COMPRESSION_GZIP:
-		//err = compressBound((uLong)compressbound);
+		deflen = g_compressbound;
 		err = compress((Bytef*)dest, (uLongf*) &deflen,
 				(const Bytef*)source, (uLongf) slen);
-		printf("%d.\n", err);
 		assert(err == Z_OK);
 		sntDebugPrintf("sntDeflate, gzip %u:%d.\n", slen, deflen);
 		assert(deflen >= 0);
