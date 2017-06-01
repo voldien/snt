86d4f3b6f10b21d5b1f190d6f58396f8e9f82cbb
Voldie <voldiekami@gmail.com>
2017-06-01 08:37:20 +0200

    Resolved bug with wrong condition logic.

--- src/debug.c
+++ src/debug.c
@@ -25,7 +25,7 @@ void sntPrintPacketInfo(const SNTUniformPacket* packet){
 					"--------------\n",
 					SNT_GET_MAJ_VERSION(packet->header.version),
 					SNT_GET_MIN_VERSION(packet->header.version),
-					packet->header.stype, sntSymbolArraySize(gs_symprotocol) <= packet->header.stype ?
+					packet->header.stype, packet->header.stype <= sntSymbolArraySize(gs_symprotocol) ?
 							gs_symprotocol[packet->header.stype] : "",
 					packet->header.offset,
 					sntDatagramSize(&packet->header),
